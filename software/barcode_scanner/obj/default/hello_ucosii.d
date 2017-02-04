e135a5af3b694b7e414c2042eb6df82626f530ed
Kyle <koshaugh@e3-11-13.ece.ualberta.ca>
2017-02-03 18:40:43 -0700

    added barcode_scanner software project, this will be a simple test of the barcode scanner and ps2 interface

---
+++ software/barcode_scanner/obj/default/hello_ucosii.d
@@ -0,0 +1,59 @@
+obj/default/hello_ucosii.o: hello_ucosii.c \
+  ../barcode_scanner_bsp//HAL/inc/includes.h \
+  ../barcode_scanner_bsp//HAL/inc/os_cpu.h \
+  ../barcode_scanner_bsp//HAL/inc/sys/alt_irq.h \
+  ../barcode_scanner_bsp//HAL/inc/nios2.h \
+  ../barcode_scanner_bsp//HAL/inc/alt_types.h \
+  ../barcode_scanner_bsp/system.h ../barcode_scanner_bsp/linker.h \
+  ../barcode_scanner_bsp//HAL/inc/priv/alt_legacy_irq.h \
+  ../barcode_scanner_bsp/system.h ../barcode_scanner_bsp//HAL/inc/nios2.h \
+  ../barcode_scanner_bsp//HAL/inc/alt_types.h \
+  ../barcode_scanner_bsp//HAL/inc/sys/alt_irq.h \
+  ../barcode_scanner_bsp//UCOSII/inc/os_cfg.h \
+  ../barcode_scanner_bsp//HAL/inc/sys/alt_alarm.h \
+  ../barcode_scanner_bsp//HAL/inc/sys/alt_llist.h \
+  ../barcode_scanner_bsp//HAL/inc/priv/alt_alarm.h \
+  ../barcode_scanner_bsp/system.h \
+  ../barcode_scanner_bsp//UCOSII/inc/ucos_ii.h \
+  ../barcode_scanner_bsp//UCOSII/inc/os_cfg.h \
+  ../barcode_scanner_bsp//HAL/inc/os_cpu.h
+
+../barcode_scanner_bsp//HAL/inc/includes.h:
+
+../barcode_scanner_bsp//HAL/inc/os_cpu.h:
+
+../barcode_scanner_bsp//HAL/inc/sys/alt_irq.h:
+
+../barcode_scanner_bsp//HAL/inc/nios2.h:
+
+../barcode_scanner_bsp//HAL/inc/alt_types.h:
+
+../barcode_scanner_bsp/system.h:
+
+../barcode_scanner_bsp/linker.h:
+
+../barcode_scanner_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+../barcode_scanner_bsp/system.h:
+
+../barcode_scanner_bsp//HAL/inc/nios2.h:
+
+../barcode_scanner_bsp//HAL/inc/alt_types.h:
+
+../barcode_scanner_bsp//HAL/inc/sys/alt_irq.h:
+
+../barcode_scanner_bsp//UCOSII/inc/os_cfg.h:
+
+../barcode_scanner_bsp//HAL/inc/sys/alt_alarm.h:
+
+../barcode_scanner_bsp//HAL/inc/sys/alt_llist.h:
+
+../barcode_scanner_bsp//HAL/inc/priv/alt_alarm.h:
+
+../barcode_scanner_bsp/system.h:
+
+../barcode_scanner_bsp//UCOSII/inc/ucos_ii.h:
+
+../barcode_scanner_bsp//UCOSII/inc/os_cfg.h:
+
+../barcode_scanner_bsp//HAL/inc/os_cpu.h:
