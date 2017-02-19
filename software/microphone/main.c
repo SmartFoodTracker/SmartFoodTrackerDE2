e65833d007b10dd23aebeeb1aac809e5a7e03f9c
kyleoshaughnessy <kyle.oshaughnessy@gmail.com>
2017-02-19 11:59:33 -0700

    Created base project for push-to-talk microphone driver

---
+++ software/microphone/main.c
@@ -0,0 +1,97 @@
+/** @file   main.c
+ *  @brief  Testing program for push-to-talk microphone
+ *
+ *  @author Kyle O'Shaughnessy (koshaugh)
+ */
+
+/*****************************************************************************/
+/* Includes                                                                  */
+/*****************************************************************************/
+
+#include <stdio.h>
+#include "includes.h"
+#include "altera_up_avalon_character_lcd.h"
+
+/*****************************************************************************/
+/* Constants                                                                 */
+/*****************************************************************************/
+
+#define TASK_STACKSIZE      2048
+#define LCD_TASK_PRIORITY   1
+
+/*****************************************************************************/
+/* Globals                                                                   */
+/*****************************************************************************/
+
+OS_STK  LCDTaskStack[TASK_STACKSIZE];
+
+/*****************************************************************************/
+/* Functions                                                                 */
+/*****************************************************************************/
+
+/**
+ * @brief       LCD update task
+ * @details
+ *
+ * @param pData
+ */
+void
+LCDTask(void* pData)
+{
+    alt_up_character_lcd_dev *pLCD = NULL;
+
+    // LCD setup
+    if ((pLCD = alt_up_character_lcd_open_dev(CHARACTER_LCD_NAME)) == NULL)
+    {
+        printf("LCD setup failed\n");
+    }
+    else
+    {
+        // Clear LCD
+        alt_up_character_lcd_init(pLCD);
+        alt_up_character_lcd_set_cursor_pos(pLCD, 0, 0);
+        alt_up_character_lcd_string(pLCD, "setup");
+    }
+
+    // Functional task loop
+    while (1)
+    {
+    	OSTimeDlyHMSM(0, 0, 1, 0);
+    }
+} // LCDTask
+
+/*****************************************************************************/
+
+/**
+ * @brief   Program entry point
+ * @details Initialize tasks and shared objects
+ */
+int
+main(void)
+{
+    INT8U status = OS_NO_ERR;
+
+    // Create and initialize LCDTask
+    status = OSTaskCreateExt(LCDTask,
+                             NULL,
+                             &LCDTaskStack[TASK_STACKSIZE-1],
+                             LCD_TASK_PRIORITY,
+                             LCD_TASK_PRIORITY,
+                             LCDTaskStack,
+                             TASK_STACKSIZE,
+                             NULL,
+                             0);
+    if (status != OS_NO_ERR)
+    {
+        printf("LCDTask setup failed.\n");
+    }
+
+    // Start OS
+    OSStart();
+
+    return 0;
+} // main
+
+/*****************************************************************************/
+/* End of File                                                               */
+/*****************************************************************************/
